8838f4511b410611b5ecdfd69d96bffa0cbad4da
mehrdad <roienyan@gmail.com>
2023-01-17 15:59:06 +0330

    deploy

--- pages/api/flights.js
+++ pages/api/flights.js
@@ -1,4 +1,4 @@
-import flightsData from "../data/flight_tehtoshz"
+import flightsData from "../../public/data/flight_tehtoshz"
 const flights = flightsData();
 export default function handler(req, res) {
     res.status(200).json(flights)
---
+++ pages/flight.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+
+type Props = {}
+
+const flight = (props: Props) => {
+  return (
+    <>
+    <Header />
+    
+    </>
+  )
+}
+
+export default flight
